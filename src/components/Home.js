007b9e3d3b521ae991884fb331b01e4235e1da76
Jeffrey E. Uvero <jeffrey.uvero@amellar.com>
2019-04-04 09:04:13 +0800

    implement home page

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react'
+
+
+class Home extends Component {
+	state = {
+
+	}
+
+	render(){
+		return (
+			<div>
+				<p>This is Home</p>
+			</div>
+		)
+	}
+} 
+
+export default Home
