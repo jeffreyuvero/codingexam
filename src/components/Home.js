556b00d3a691dc1e431bc12dd3209d8f4e6eb817
Jeffrey E. Uvero <jeffrey.uvero@amellar.com>
2019-04-04 09:14:53 +0800

    put static data for testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,33 @@ import React , {Component} from 'react'
 
 class Home extends Component {
 	state = {
-
+		'title' : [
+			'San Diego' , 'San Francisco', 'Oklahoma City'
+		]
 	}
 
 	render(){
+		const titles = this.state.title; 
 		return (
 			<div>
-				<p>This is Home</p>
+				<h1>Location </h1>
+				<div className = "row">
+					<div className = "col-lg-9">
+						<input type="text" class="form-control" id="usr" placeholder = "Search for the location" />
+					</div>
+				</div>
+				<hr />
+				<div className = "row">
+					<div className = "col-lg-9">
+						{titles.map(title => (
+							<div class="card">
+							  <div class="card-body">
+							     <a href= "#">{ title }</a>
+							  </div>
+							</div>
+						))}	
+					</div>
+				</div>
 			</div>
 		)
 	}
